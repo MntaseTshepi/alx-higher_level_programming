eb2464c6423d42f1e15769f76e82ca74ca485389
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-06-06 13:34:32 -0700

    Updated Task 15: Requirement fail

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -4,5 +4,5 @@ def remove_char_at(str, n):
     str_cpy = "" + str
     if len(str_cpy) < n or n < 0:
         return str_cpy
-    str_cpy = str_cpy.replace(str_cpy[n],"")
+    str_cpy = str_cpy.replace(str_cpy[n], "")
     return str_cpy
