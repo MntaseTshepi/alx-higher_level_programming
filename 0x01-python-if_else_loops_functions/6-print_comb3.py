ffae97577e6136c6f2ae924365586544435966e1
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-06-06 10:22:34 -0700

    Updated task 6, 7 and 8 : Requirement fail

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for val in range(0, 10):
     for val2 in range(val+1, 10):
         if val != val2:
             if val == 8 and val2 == 9:
-                print("{}{}".format(val,val2))
+                print("{}{}".format(val, val2))
             else:
-                print("{}{}".format(val,val2), end=", ")
+                print("{}{}".format(val, val2), end=", ")
