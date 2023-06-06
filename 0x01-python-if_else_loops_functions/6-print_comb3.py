8ad1ceafad79eccd7846803715bda6c25d73e9b8
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-06-06 10:14:04 -0700

    Task 6 - prints combinations of two digits | Task 7 -checks lowecase character | Task 8 - prints uppercase

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+for val in range(0, 10):
+    for val2 in range(val+1, 10):
+        if val != val2:
+            if val == 8 and val2 == 9:
+                print("{}{}".format(val,val2))
+            else:
+                print("{}{}".format(val,val2), end=", ")
