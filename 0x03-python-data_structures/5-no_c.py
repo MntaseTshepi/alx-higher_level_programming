91a7d49d4dd8bb9cfb985f4d9dd86b619ff95330
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-06-10 14:55:09 -0700

    Updated Task 5 : Requirement fail

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 def no_c(my_string):
 
     """Removes all characters c and C from a string"""
 
     new_string = ""
-
     for letter in my_string:
         if letter != 'c' and letter != 'C':
             new_string += letter
-
     return new_string
