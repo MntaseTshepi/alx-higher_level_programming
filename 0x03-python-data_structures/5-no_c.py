92e6c5f9f81aba148cc91da1f75b1bd87832a9c2
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-06-10 14:32:59 -0700

    Updated Task 5 : Requirement failure

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -7,8 +7,7 @@ def no_c(my_string):
     new_string = ""
 
     for letter in my_string:
-        if letter == 'c' or letter == 'C':
-            continue
-        new_string += letter
+        if letter != 'c' and letter != 'C':
+            new_string += letter
 
     return new_string
