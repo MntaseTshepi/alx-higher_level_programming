63f857f69829a047f945b2485fcbcb824aa008e7
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-06-11 08:51:25 -0700

    Updated Task 8 : Output fail

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@
 def multiple_returns(sentence):
 
     """Returns the length of the character and its first character"""
-    if sentence is None:
+    if sentence == "":
         return (0, None)
     return (len(sentence), sentence[0])
