6aaa460bbd096f348e73632e9e3763e1d0128899
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-06-14 13:21:36 -0700

    Updayed Task 10: Code error fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,6 +5,6 @@ def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
 
-    max_val = max(a_dictionary)
+    max_val = max(a_dictionary, key=a_dictionary.get)
 
     return max_val
