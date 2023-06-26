24ee2a926ee4120a8c2763aba3d1a1d6704bc93b
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-06-26 07:21:20 -0700

    Updated Task 0,1,2 - Requirement fail

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,8 +3,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return (True)
-    except ValueError:
-        return (False)
-    except TypeError:
-        return(False)
+        return True
+    except (ValueError, TypeError):
+        return False
