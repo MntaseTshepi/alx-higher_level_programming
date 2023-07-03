1f7ddbdb6632de1c59b758b182bd707390d155de
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-07-03 10:09:03 -0700

    Task 0 - Simple rectangle | Task 1 - Definition of a rectangle |  Task 2 - Area and Perimeter | Task 3 - String representation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""Defines a rectangle class"""
+
+
+class Rectangle:
+    """Represents a rectangle class"""
+    def __init__(self, width=0, height=0):
+        """Initializes a rectangle
+        Args:
+            width(int): The width of a rectangle.
+            height(int): The height of a rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get/set the width of the attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set the height of the attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def perimeter(self):
+        """Returns the perimeter of a rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return((2 * self.__width) + (2 * self.__height))
+
+    def area(self):
+        """Returns the area of a rectangle"""
+        return(self.__width * self.__height)
+
+        """Prints out a square with #"""
+    def __str__(self):
+        """Returns the representaion of the rectangle using #"""
+
+        if self.__width == 0 or self.__height == 0:
+            return("")
+        rec = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                rec += '#'
+            if i != self.__height - 1:
+                rec += '\n'
+        return (rec)
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,7 @@
+0x08. Python - More Classes and Objects
+
+Task 0 - Writing an empty class Rectangle that defines a rectangle.
+Task 1 - Writing a class Rectangle that defines a rectangle with private instances width and height.
+Task 2 - Writing a class Rectangle that defines a rectangle with public instance methods area and perimeter.
+Task 3 - Writing a class Rectangle that defines a rectangle and prints the rectangle using the # character.
+Task 4 - Return a string representation of the rectangle to be able to recreate a new instance by using eval().
